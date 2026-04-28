d855e90903f2e11fb80cf41ed93d3dde2c47e95d
Mihasintsoa <mihasintsoarakotondraibe@gmail.com>
2026-04-28 23:29:20 +0200

    added Spring Boot Security on log in

--- src/main/java/org/example/MainView.java
+++ src/main/java/org/example/MainView.java
@@ -3,12 +3,14 @@ package org.example;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import org.example.Checking.Checking;
 
 @Route
 @AnonymousAllowed
+@PageTitle("Redirection")
 public class MainView extends VerticalLayout implements BeforeEnterObserver {
 
     @Override
